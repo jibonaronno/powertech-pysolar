fb2001009d1f25a8762260c6e2b6b2c5aaf619b7
jibonaronno <tebunia@gmail.com>
2020-07-29 04:33:18 +0600

    Adding support for call to nervoustech.com?key=value&key=value

--- main.py
+++ main.py
@@ -190,17 +190,17 @@ class MainWindow(QMainWindow):
             #self.lcdinvvolt01.display(self.invVoltage01)
             
             #unpack does not work for 2 bytes
-            print(str(struct.unpack('!i', bytes.fromhex('00'+ttr[3] + ttr[4]))[0]))
+            print(str(struct.unpack('!i', bytes.fromhex( b'0'+b'0'+ttr[3] + ttr[4]))[0]))
 
             #Read 4 byte into 32 bits integer value
             #print(str(struct.unpack('!i', bytes.fromhex(ttr[3] + ttr[4] + ttr[5] + ttr[6]))[0]))
             
             #self.lcdkwhinv.display(struct.unpack('!i', bytes.fromhex(ttr[3] + ttr[4] + ttr[5] + ttr[6]))[0])
 
-            self.PV1volt = str(struct.unpack('!i', bytes.fromhex( '00' +ttr[3] + ttr[4]))[0])
+            self.PV1volt = str(struct.unpack('!i', bytes.fromhex( b'0'+b'0' +ttr[3] + ttr[4]))[0])
             
             #Read 16bit integer
-            self.lcdkwhinvout.display(struct.unpack('!i', bytes.fromhex( '00' + ttr[3] + ttr[4]))[0])
+            self.lcdkwhinvout.display(struct.unpack('!i', bytes.fromhex( b'0'+b'0' + ttr[3] + ttr[4]))[0])
 
             #Read 32Bit integer
             #self.lcdkwhinvout.display(struct.unpack('!i', bytes.fromhex(ttr[3] + ttr[4] + ttr[5] + ttr[6]))[0])
